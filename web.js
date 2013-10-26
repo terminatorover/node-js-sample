72681107d2d470b504e3b9d622b1e7dabac5f8fb
Ubuntu <ubuntu@ip-172-31-1-90.us-west-2.compute.internal>
2013-10-26 15:45:03 +0000

    tried to specify html file

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    var file_in_buffer = fs.readFileSync('index.html');
+    var file_in_buffer = fs.readFileSync('/index.html');
     
     var file_as_string = buf.toString(file_in_buffer);
 
